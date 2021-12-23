c2d9b9c3923372d188f47c47d2ad4499ecaa980d
the-halfbloodprince <ammanishkumardas@gmail.com>
2021-12-24 02:48:51 +0530

    fixed few indentation and schema issues

--- server/models/profile.js
+++ server/models/profile.js
@@ -4,30 +4,30 @@ const mongoose = require('mongoose')
 // const { externalProfileLinksSchema } = require('../models/externalProfileLinks')
 
 const profileSchema = new mongoose.Schema({
-    id:          { type: String, required: true },
-    avatar_url:  { type: String },
-    name:        { type: String, required: true },
-    company:     { type: String },
-    blog:        { type: String },
-    location:    { type: String },
-    email:       { type: String },
-    bio:         { type: String },
+    id:                   { type: String, required: true },
+    avatar_url:           { type: String },
+    name:                 { type: String, required: true },
+    company:              { type: String },
+    blog:                 { type: String },
+    location:             { type: String },
+    email:                { type: String },
+    bio:                  { type: String },
  // externalProfileLinks: { type: externalProfileLinksSchema },
     externalProfileLinks: { type: {
-                        github_id:       { type: String },
-                        linkedin_id:     { type: String },
-                        codechef_id:     { type: String },
-                        hackerrank_id:   { type: String },
-                        twitter_id:      { type: String },
-                        medium_id:       { type: String },
-                 }},
- // repos:       { type: [githubRepoSchema] },
-    repos:       { type: [{
-                        name:           { type: String, required: true },
-                        html_url:       { type: String, required: true },
-                        description:    { type: String, required: true },
-                        updated_at:     { type: String, required: true },
-                 }]},
+                                github_id:       { type: String },
+                                linkedin_id:     { type: String },
+                                codechef_id:     { type: String },
+                                hackerrank_id:   { type: String },
+                                twitter_id:      { type: String },
+                                medium_id:       { type: String },
+                            }},
+ // repos:                { type: [githubRepoSchema] },
+    repos:                { type: [{
+                                name:           { type: String, required: true },
+                                html_url:       { type: String, required: true },
+                                description:    { type: String, required: true },
+                                updated_at:     { type: String },
+                            }]},
 
 })
 
@@ -45,9 +45,9 @@ const validate = (profile) => {
     })
 
     const githubRepo =  Joi.object().keys({
-        name:           Joi.string(),
-        html_url:       Joi.string(),
-        description:    Joi.string(),
+        name:           Joi.string().required(),
+        html_url:       Joi.string().required(),
+        description:    Joi.string().required(),
         updated_at:     Joi.string(),
     })
 
