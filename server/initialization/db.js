41a7600d1e418f2934ae553172830bab6f7ff7a6
the-halfbloodprince <ammanishkumardas@gmail.com>
2021-12-24 02:17:08 +0530

    security update

--- server/initialization/db.js
+++ server/initialization/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const DATABASE_CONNECTION_STRING = 'mongodb+srv://admin:cadbury-devprofile-admin@devprofile.ftrgk.mongodb.net/dev?retryWrites=true&w=majority'
+const DATABASE_CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING
 
 const initializeDB = () => {
     mongoose.connect(DATABASE_CONNECTION_STRING, 
