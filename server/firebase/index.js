116a59677ef5b756170623143fd5048c42004b67
Procheta Bhattacharyya <53190270+procheta1999@users.noreply.github.com>
2021-12-31 22:11:03 +0530

    Revert "Master"

--- server/firebase/index.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin')
-const credentials = require('../config/credentials.json')
-
-// Initialize the app with a service account, granting admin privileges
-admin.initializeApp({
-    credential: admin.credential.cert(credentials),
-})
-
-module.exports = admin
